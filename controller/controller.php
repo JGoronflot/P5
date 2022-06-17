d61ff721232fdc45ef8c5a5eab03f1c50b5f4ad7
Jimmy Goronflot <jgoronflot20@gmail.com>
2022-06-17 15:14:57 +0200

    test fix

--- controller/controller.php
+++ controller/controller.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once('model/PostManager.php');
-require_once('model/CommentManager.php');
-require_once('model/UserManager.php');
-require_once('model/MailManager.php');
+require_once 'model/PostManager.php';
+require_once 'model/CommentManager.php';
+require_once 'model/UserManager.php';
+require_once 'model/MailManager.php';
 
 function listHomePosts(){
 
